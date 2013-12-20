650ec1437ce5af8adfec129508e70c3e8019e03b
danapczynski <apczynski@gmail.com>
2013-12-20 12:44:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opus::Application.config.secret_token = '3c8cac413ab14c2224022f7b0104e3a547ac1a62aaa2704a1527b7c2081b22cc957fc31e17c17e1a68a2a7b2ce0ac51ba54580a66e8faf03bfd4883c7bba678a'
