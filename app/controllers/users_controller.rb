449a21b83e68590769287030bcde5381465f166f
danapczynski <apczynski@gmail.com>
2013-12-21 23:52:49 -0800

    added error handling to users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      login(@user)
       redirect_to @user
     else
+      @errors = @user.errors.full_messages
       render 'new'
     end
   end
