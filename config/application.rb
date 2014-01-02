3499600e70182908011afccf01cf999eea50b9b8
danapczynski <apczynski@gmail.com>
2014-01-02 11:44:01 -0800

    added new JS files to assets precompile

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,8 @@ module Opus
     # Avoiding assets:precompile hangups in Heroku
     config.assets.initialize_on_precompile = false
     config.assets.precompile += ['forms.js']
+    config.assets.precompile += ['blocks.js']
+    config.assets.precompile += ['demo.js']
     config.assets.precompile += ['levels.js']
     config.assets.precompile += ['levels/*.js']
   end
