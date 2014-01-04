dedf1b7cd52a54a8f41905332a05c1a8a5c2172d
danapczynski <apczynski@gmail.com>
2014-01-03 19:38:34 -0800

    Revert "renamed User#report_levels to User#available_levels, and tweaked it to include return value of User#next_level"
    This reverts commit 5643824cf9bf18b9101066a24b33d50dc5e7b499.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,8 @@ class User < ActiveRecord::Base
     Level.find_by_level_number(last_level_completed.level_number + 1)
   end
 
-  def available_levels
-    next_level ? levels.uniq + [next_level] : levels.uniq
+  def report_levels
+    levels.uniq
   end
 
   def best_score(level)
